0de7870b06eeb5ed88336df054a52343a64bb1ba
Luke Ryan <github@lukeryan.net.nz>
2020-11-26 21:13:11 +1300

    Deploy script builds and invalidates cdn.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@
 # Uploads the contents of the current directory to the production S3 bucket. 
 # Requires the AWS CLI configured with AWS credentials. See https://aws.amazon.com/cli/
 
-aws s3 sync . s3://splitlightglass.com --region ap-southeast-2 --exclude ".git/*"
+hugo -D
+
+aws s3 sync ./public s3://splitlightglass.com --region ap-southeast-2 --exclude ".git/*"
+
+aws cloudfront create-invalidation --distribution-id EQ9UK8VMORZRD --paths "/index.html" "/"
